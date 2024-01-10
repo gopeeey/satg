2e1501599702aba777a96f03632e66350a564fd2
Samuel Gopeh <sammygopeh@gmail.com>
2024-01-11 00:53:07 +0100

    deleted build folder

--- build/races/service/events.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.raceEvents = void 0;
-exports.raceEvents = {
-    join: "race:join",
-    newPlayer: "race:new-player",
-    playerInput: "race:player-input",
-    playerUpdate: "race:player-update",
-};
