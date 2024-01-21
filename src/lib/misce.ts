50b045a5600a223abdbdaef57670586f749c4abc
Samuel Gopeh <sammygopeh@gmail.com>
2024-01-21 01:15:19 +0100

    uncommented the remaining excepts

--- src/lib/misce.ts
+++ src/lib/misce.ts
@@ -16,61 +16,61 @@ export const avatarArray = [
 ];
 
 export const excerpts: ExcerptType[] = [
-  // {
-  //   title: "Jane Eyre",
-  //   author: "Charlotte Bronte",
-  //   body: "A lover finds his mistress asleep on a mossy bank; he wishes to catch a glimpse of her fair face without waking her. He steals softly over the grass, careful to make no sound; he pauses-fancying she has stirred : he withdraws; not for worlds would he be seen. All is still: he again advances: he bends above her; a light veil rests on her features: he lifts it, bends lower; now his eyes anticipate the vision of beauty-warm, and blooming, and lovely, in rest. How hurried was their first glance! But how they fix! How he starts! How he suddenly and vehemently clasps in both arms the form he dared not, a moment since, touch with his finger! How he calls aloud a name, and drops his burden, and gazes on it wildly! He thus grasps and cries, and gazes, because he no longer fears to waken by any sound he can utter-by any movement he can make. He thought his love slept sweetly: he finds she is stonedead.",
-  //   year: 1899,
-  // },
+  {
+    title: "Jane Eyre",
+    author: "Charlotte Bronte",
+    body: "A lover finds his mistress asleep on a mossy bank; he wishes to catch a glimpse of her fair face without waking her. He steals softly over the grass, careful to make no sound; he pauses-fancying she has stirred : he withdraws; not for worlds would he be seen. All is still: he again advances: he bends above her; a light veil rests on her features: he lifts it, bends lower; now his eyes anticipate the vision of beauty-warm, and blooming, and lovely, in rest. How hurried was their first glance! But how they fix! How he starts! How he suddenly and vehemently clasps in both arms the form he dared not, a moment since, touch with his finger! How he calls aloud a name, and drops his burden, and gazes on it wildly! He thus grasps and cries, and gazes, because he no longer fears to waken by any sound he can utter-by any movement he can make. He thought his love slept sweetly: he finds she is stonedead.",
+    year: 1899,
+  },
 
-  // {
-  //   title: "Into the Wild",
-  //   author: "Jon Krakauer",
-  //   body: "He was unheeded, happy, and near to the wild heart of life. He was alone and young and wilful and wildhearted, alone amid a waste of wild air and brackish waters and the seaharvest of shells and tangle and veiled grey sunlight.",
-  // },
+  {
+    title: "Into the Wild",
+    author: "Jon Krakauer",
+    body: "He was unheeded, happy, and near to the wild heart of life. He was alone and young and wilful and wildhearted, alone amid a waste of wild air and brackish waters and the seaharvest of shells and tangle and veiled grey sunlight.",
+  },
 
   {
     author: "Albert Einstein",
     body: "Our task must be to free ourselves... by widening our circle of compassion to embrace all living creatures and the whole of nature and it's beauty.",
   },
 
-  // {
-  //   title: "Great Expectations",
-  //   author: "Charles Dickens",
-  //   body: `We went on in this way for a long time, and it seemed likely that we should continue to go on in this way for a long time, when one day Miss Havisham stopped short as she and I were walking, she leaning on my shoulder; and said with some displeasure,- "You are growing tall, Pip!" I thought it best to hint, through the medium of a meditative look, that this might be occasioned by circumstances over which I had no control.`,
-  //   year: 2010,
-  // },
+  {
+    title: "Great Expectations",
+    author: "Charles Dickens",
+    body: `We went on in this way for a long time, and it seemed likely that we should continue to go on in this way for a long time, when one day Miss Havisham stopped short as she and I were walking, she leaning on my shoulder; and said with some displeasure,- "You are growing tall, Pip!" I thought it best to hint, through the medium of a meditative look, that this might be occasioned by circumstances over which I had no control.`,
+    year: 2010,
+  },
 
-  // {
-  //   title: "Great Expectations",
-  //   author: "Charles Dickens",
-  //   body: `"And then, dear boy, it was a recompense to me, look'ee here, to know in secret that I was making a gentleman. The blood horses of them colonists might fling up the dust over me as I was walking; what do I say? I says to myself, 'I'm making a better gentleman nor ever you'll be! ... If I ain't a gentleman, nor ain't got no learning, I'm the owner of such. All on you owns stock and land; which on you owns a brought-up London gentleman?'"`,
-  //   year: 2010,
-  // },
+  {
+    title: "Great Expectations",
+    author: "Charles Dickens",
+    body: `"And then, dear boy, it was a recompense to me, look'ee here, to know in secret that I was making a gentleman. The blood horses of them colonists might fling up the dust over me as I was walking; what do I say? I says to myself, 'I'm making a better gentleman nor ever you'll be! ... If I ain't a gentleman, nor ain't got no learning, I'm the owner of such. All on you owns stock and land; which on you owns a brought-up London gentleman?'"`,
+    year: 2010,
+  },
 
-  // {
-  //   title: "The Subtle Art of Not Giving a F*ck",
-  //   author: "Mark Manson",
-  //   body: `"Many people may be to blame for your unhappiness, but nobody is ever responsible for your unhappiness but you. This is because you always get to choose how you see things, how you react to things, how you value things. You always get to choose the metric by which to measure your experiences."`,
-  //   year: 2016,
-  // },
+  {
+    title: "The Subtle Art of Not Giving a F*ck",
+    author: "Mark Manson",
+    body: `"Many people may be to blame for your unhappiness, but nobody is ever responsible for your unhappiness but you. This is because you always get to choose how you see things, how you react to things, how you value things. You always get to choose the metric by which to measure your experiences."`,
+    year: 2016,
+  },
 
-  // {
-  //   author: "Pierre Teilhard de Chardin",
-  //   body: "Someday, after mastering the winds, the waves, the tides and gravity, we shall harness for God the energies of love, and then, for a second time in the history of the world, man will have discovered fire.",
-  // },
+  {
+    author: "Pierre Teilhard de Chardin",
+    body: "Someday, after mastering the winds, the waves, the tides and gravity, we shall harness for God the energies of love, and then, for a second time in the history of the world, man will have discovered fire.",
+  },
 
-  // {
-  //   author: "Adam Riess",
-  //   body: "One of the most exciting things about dark energy is that it seems to live at the very nexus of two of our most successful theories of physics: quantum mechanics, which explains the physics of the small, and Einstein's Theory of General Relativity, which explains the physics of the large, including gravity.",
-  // },
+  {
+    author: "Adam Riess",
+    body: "One of the most exciting things about dark energy is that it seems to live at the very nexus of two of our most successful theories of physics: quantum mechanics, which explains the physics of the small, and Einstein's Theory of General Relativity, which explains the physics of the large, including gravity.",
+  },
 
-  // {
-  //   title: "Please Look After Mom",
-  //   author: "Kyung-Sook Shin",
-  //   body: "All I wanted was for you to be free from everything. And with that freedom, you often showed me another world, so I wanted you to be even freer. I wanted you to be so free that you would live your life for other people.",
-  //   year: 2008,
-  // },
+  {
+    title: "Please Look After Mom",
+    author: "Kyung-Sook Shin",
+    body: "All I wanted was for you to be free from everything. And with that freedom, you often showed me another world, so I wanted you to be even freer. I wanted you to be so free that you would live your life for other people.",
+    year: 2008,
+  },
 
   // {
   //   title: "Just a test text",
